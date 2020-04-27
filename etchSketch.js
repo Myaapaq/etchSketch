da3c7a83bac1c762a27b6110942beaf525b5f046
Myaapaq <myapaq@gmail.com>
2020-04-27 14:05:42 -0500

    Polish styles, add user input abilities.
    clear

--- etchSketch.js
+++ etchSketch.js
@@ -1,5 +1,5 @@
 const etchDiv = document.querySelector('#etchDiv')
-//draws the blocks
+//Creates the grid to specified size
 function blockMaker(squareNum){
 var i;
    for (i = 0; i < squareNum; i++) {
@@ -10,9 +10,9 @@ var i;
       etchDiv.appendChild(grid);
    }
 }
-//function for size selection 
-   let squareSize;
-   const etchDivArea = 860;
+//Runs equation to determine size of each grid 
+let squareSize;
+const etchDivArea = 860;
 function findGridSize(squaresPerSide){
    squareNum = squaresPerSide * squaresPerSide;
    squareSize = etchDivArea / squaresPerSide;
@@ -25,6 +25,7 @@ function findGridSize(squaresPerSide){
       console.log(squareSize);
       console.log(squareNum);
 }
+//Applys the specified size to the grids
 function setGridSize(squareSize){
    blocks = document.getElementsByClassName("blocks");
    var b;
@@ -36,60 +37,56 @@ function setGridSize(squareSize){
    blocks[b].style.width = squareSize;
    }
 }
+//clears the grids out of the etchDiv element 
+function reset(){
+   while (etchDiv.hasChildNodes()){
+     etchDiv.removeChild(etchDiv.firstChild);
+ };
+};
+//Clears previous grid and makes new one to specified size
 function newGrid(squaresPerSide){
+  
+   if (squaresPerSide == 26){
+      squaresPerSide = 25;
+   }
+   /*if (squaresPerSide != ){
+      alert(typeof squaresPerSide + squaresPerSide )
+      getUserInput();
+      convertToNumber(squaresPerSide);
+   }*/
+
    reset();
    findGridSize(squaresPerSide);
    blockMaker(squareNum)
    setGridSize(squareSize);
-   
 } 
-function reset(){
-     while (etchDiv.hasChildNodes()){
-       etchDiv.removeChild(etchDiv.firstChild);
-   };
-};
+function convertToNumber(squaresPerSide){
+   console.log(typeof Number(squaresPerSide));
+}
+function getUserInput(){
+squaresPerSide = prompt("How many grids per side would you like? Value must be less than 25")
+}
+//buttons
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => {
+  alert('clear');
+});
+let squaresPerSide;
+const size = document.querySelector('#size');
+size.addEventListener('click', function(){
+   
+   getUserInput();
+   newGrid(squaresPerSide);
+});
 
-//how to 'clear' so the divs dont stack onto what is already there. Set everything to zero?
 
 
 
 /*
-Formula for the grid size calc:
-p= pixel size
-a= area
-n= number of squares
-p= a/n
+----SCRAPS----
 
-NOTES: 
-What needs to be done -->
-   change the width and height of the .block elements
-   using a function call with parameters for now
-      eventually run a function via the button that collects user imput and runs the newGrid function
-
-skeleton of what I need to do to change the width and height for all the grids.
-1.) run the function that gives us the number for the width and height 
-      store that in a variable.
-2.) Insert that variable into the style of the element
-3.) because there is more than one grid in the class we need to run through all of them with a loop. This needs to be done for both the width and the height. 
-         var x = document.getElementsByClassName("blocks");
-      var i;
-      for (i = 0; i < x.length; i++) {
-      x[i].style.width = squareSize;
-      }
-4.) relode the page? Restart it or something possibly to get the changes to work
-5.) Hope to god that it works? lol
-ONCE ALL THAT WORKS...
-      get it to work at the press of a button with user input from a prompt.
-      look at the rock paper scissors game to remember how to switch from using a function parameter to subbing in the input from the prompt.
-      I think I just assign the prompt input to the place holder variable (squareNum) that was the function parameter and go on with life if I remember right. 
-
-THINGS I NEED TO LEARN
-      How to run a function with a button press
-      possibly... how to relode the page? in a function
-------------SCRAPS:------------
-<h1>Etch-a-Sketch!</h1>
-background-color:rgb(239, 0, 0);
 */
+
 
 
 
